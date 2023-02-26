188b46680c18e220cc4852d10719c66ad2b5b9ce
Vincent M <maladiere.vincent@yahoo.fr>
2023-02-26 23:58:22 +0100

    update init

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
-from src.utils.gcp.actions import (
+from .actions import (
     gcp_fetch,
     gcp_push,
     gcp_query,
     store_locally,
     load_locally,
 )
-from src.utils.gcp.table import Table
+from .table import Table
 
 __all__ = [gcp_push, gcp_fetch, gcp_query, store_locally, load_locally, Table]
